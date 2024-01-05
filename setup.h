5dc139e8f88bf96bb0b77f63d93a32aa554b0b39
Wijdan <34543247+wijdanmkh-insk@users.noreply.github.com>
2024-01-05 22:31:34 +0700

    Update setup.h

--- setup.h
+++ setup.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #include <string.h>
@@ -12,28 +13,50 @@ void setup(){
     system("cls");
     struct users input;
     struct users check;
+    char confirm[300];
 
+    bool match;
+    bool not_blank;
+
+    printf("================================ SETUP AKUN ================================\n");
     printf("Selamat datang di menu setup aplikasi kasir\n");
-    printf("Tahapan yang akan dilewati :\n1. Membuat direktori dan file pengguna\n2. Setup akun admin\n");
+    printf("Tahapan yang akan dilewati : Setup akun admin\n");
+    printf("\nWARNING :\nPASTIKAN ANDA MENCATAT USERNAME DAN PASSWORD KARENA SETELAH ANDA TERDAFTAR,\nDATA YANG SUDAH TERSIMPAN TIDAK DAPAT DIUBAH LAGI\n");
 
     FILE *data_admin;
     data_admin = fopen("data_admin.dat", "wb");
 
-    printf("Setup akun admin\n");
-    printf("Masukkan nickname : "); gets(input.nickname);
-    printf("Masukkan username : "); gets(input.username);
-    printf("Masukkan password : "); gets(input.password);
-    fwrite(&input, sizeof(input), 1, data_admin);
+    match = false;
+    not_blank = false;
+    printf("Masukkan nickname            : "); gets(input.nickname);
+    printf("Masukkan username            : "); gets(input.username);
 
-    printf("Setup aplikasi berhasil! Silahkan login");
+    do{ 
+        printf("Masukkan password            : "); gets(input.password);
+        printf("Masukkan konfirmasi password : "); gets(confirm);
+        if(strcmp(input.password, confirm)!= 0){
+            match = true;
+            system("cls");
+            printf("Password dan konfirmasinya tidak cocok!\n");
+            printf("Masukkan nickname            : %s\n", input.nickname); 
+            printf("Masukkan username            : %s\n", input.username); 
+        }else{
+            if(fwrite(&input, sizeof(input), 1, data_admin) != 0){
+                match = false;
+                printf("Setup aplikasi berhasil! Tekan apapun untuk melanjutkan...");
+            }
+        }
+    }while(match);
     
+    getchar();
     fclose(data_admin);
 }
 
 void pilih_role(){
     system("cls");
+    printf("================================ MENU LOGIN ================================\n");
     printf("Selamat datang! Anda akan masuk sebagai :\n");
-    printf("1. Cashier\n2. Logistician\n3. Manager\n4. Admin\nSilahkan pilih : "); int pilih; scanf("%d", &pilih); getchar();
+    printf("1. Cashier\n2. Logistician\n3. Manager\n4. Admin\n5. KELUAR\nSilahkan pilih : "); int pilih; scanf("%d", &pilih); getchar();
 
     switch(pilih){
         case 1 :
@@ -43,11 +66,13 @@ void pilih_role(){
         login_logistician(limit);
         break;
         case 3 : 
-        login_admin(limit);
+        login_manager(limit);
         break;
         case 4 :
         login_admin(limit);
         break;
+        case 5 :
+        exit(0);
         default : 
         printf("Pilihan salah!");
         pilih_role();
